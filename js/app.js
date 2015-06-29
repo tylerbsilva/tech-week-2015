88c4fdbe07f44d24a7e429caf977557ca728c67e
Tyler Silva <tylerbsilva@gmail.com>
2015-06-29 11:03:55 -0500

    Fixed injection

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 angular.module('thankYouApp', [])
 
   .controller('thankYouController', ['$scope', function($scope){
-    $scope.recruiter = {
-      name: 'naomi',
-      message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    };
+    $scope.userName = 'naomi';
+    $scope.message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
   }])
 
   .controller('basicInfoController', ['$scope', function($scope){
@@ -29,20 +27,20 @@ angular.module('thankYouApp', [])
   .directive('thankYou', function(){
     return {
       restrict: 'E',
-      templateURL: '/js/templates/thank-you.html'
+      templateUrl: '/js/templates/thank-you.html'
     };
   })
 
   .directive('basicInfo', function(){
     return {
       restrict: 'E',
-      templateURL: '/js/templates/basic-info.html'
+      templateUrl: '/js/templates/basic-info.html'
     };
   })
 
   .directive('socialMedia', function(){
     return {
       restrict: 'E',
-      templateURL: '/js/templates/social-media.html'
+      templateUrl: '/js/templates/social-media.html'
     };
   });
